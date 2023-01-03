6b166397dc89742188b93b939b083be3c1f91fc8
Tania Lopez <77525392+tanialopezPT@users.noreply.github.com>
2023-01-02 23:39:00 -0800

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,17 @@
+let todos = [
+  "clean room",
+  "brush teeth",
+  "exercise"];
+
+for (let i=0; i<todos.length; i++){
+  console.log(todos[i] + '!'}
+              
+              
+let counterOne = 10;
+              while (counterOne >0 {
+              console.log(counterOne);
+  counterOne++;
+  
+
+
+     
