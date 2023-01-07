2ec87ccf4a0f8a85cec0687d0ed63265a40ba5fd
Tania Lopez <77525392+tanialopezPT@users.noreply.github.com>
2023-01-06 22:41:03 -0800

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -3,3 +3,40 @@ document.querySelector("li").style.background="yellow"
 document.querySelector("h1").className="new class name"
 
 document.querySelector('li').classList;
+
+
+add item:
+
+let button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", function () {
+  console.log("click!!");})
+
+let button = document.getElementById("enter");
+let input= sicument.getElementById("userinput");
+let ul = document.querySelector("ul");
+
+button.addEventListener("click", function(){
+  
+  if(input.value.length > 0) {
+          let li = document.createElement("li");
+          li.appendChild(document.createTextNode(input.value));
+          ul.appendChild(li);
+          input.value=""; 
+  })
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
